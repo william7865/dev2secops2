4067981b68ab7006241b49636efeabf9f116aad2
William Lin <linwilliam14@gmail.com>
2025-03-25 14:59:20 +0100

    Ajout d'un nouvelle voiture

--- src/main/java/com/example/dev2secops2/ServiceWeb.java
+++ src/main/java/com/example/dev2secops2/ServiceWeb.java
@@ -20,6 +20,8 @@ public class ServiceWeb {
         cars.add(car);
         car = new Car("CC33DD", "peugeot", 500);
         cars.add(car);
+        car = new Car("DD44EE", "Lancia", 300);
+        cars.add(car);
     }
 
 
